d24bf5aa39cbdf91513f13a08da61c1830a0fbea
Andreas Madsen <amwebdk@gmail.com>
2015-09-30 20:41:39 +0200

    [example] print time spend for each table update.

--- example/run.js
+++ example/run.js
@@ -19,15 +19,22 @@ const settings = {
 dawaSignature(function (err, schema) {
   if (err) throw err;
 
+  let lastTime = null;
+
   // Setup replicator
   const replicate = new Replicate()
     .on('new-version', function (version) {
       console.error('updateing to version: ' + version);
       console.log('\nupdateing to version: ' + version);
     })
-    .on('update-table', function (tableName) {
+    .on('update-table-start', function (tableName) {
+      lastTime = process.hrtime();
       console.error('updateing table: ' + tableName);
       console.log('\nupdateing table: ' + tableName);
+    })
+    .on('update-table-end', function (tableName) {
+      const took = process.hrtime(lastTime);
+      console.error('  took: ' + (took[0] * 1e3 + took[1] * 1e-6) + ' ms');
     });
 
   const insert = new InsertStream(schema, settings)
